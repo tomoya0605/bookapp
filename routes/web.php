ad3778d5e57dbf637b579bbd6257b1c805bb5e98
tomoya0605 <t.tomoya0605@gmail.com>
2020-12-19 09:13:19 +0900

    commit

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Book;
+use Illuminate\Http\Request;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,5 +15,27 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $books = Book::all();
+    return view('books', ['books' => $books ]);
+})->middleware('auth');
+
+Route::post('/book', function(Request $request) {
+    $validator = Validator::make($request->all(),[
+        'name' => 'required|max:255',
+    ]);
+
+    $book = new Book;
+    $book ->title = $request->name;
+    $book->save();
+
+    return redirect('/');
 });
+
+Route::delete('/book/{book}', function(Book $book) {
+    $book->delete();
+
+    return redirect('/');
+});
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
