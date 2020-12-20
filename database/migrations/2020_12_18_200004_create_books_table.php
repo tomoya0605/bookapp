6d0aba9533ffa946ab483c1b95006c11e2bb8866
tomoya0605 <t.tomoya0605@gmail.com>
2020-12-21 07:35:56 +0900

    commit

--- database/migrations/2020_12_18_200004_create_books_table.php
+++ database/migrations/2020_12_18_200004_create_books_table.php
@@ -15,7 +15,7 @@ class CreateBooksTable extends Migration
     {
         Schema::create('books', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('title');
+            $table->string(‘title’);
             $table->timestamps();
         });
     }
